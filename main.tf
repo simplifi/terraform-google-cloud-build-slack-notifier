cf54df58dbdae85d31d02b32e943a1f28b662166
Charlie Jones <cjonesy@protonmail.com>
2021-06-24 16:25:38 -0500

    Secret cleanup, PR feedback

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 # Cloud Build Slack Notifier
 
+locals {
+  secret_id_provided = var.slack_webhook_url_secret_id != ""
+  secret_id          = local.secret_id_provided ? var.slack_webhook_url_secret_id : google_secret_manager_secret.cloud_build_slack_webhook_url.id
+  clean_name         = lower(replace(var.name, "/[^0-9A-Za-z]/", "-"))
+}
+
 # Enable required APIs
 resource "google_project_service" "apis" {
   for_each = toset([
@@ -9,7 +15,7 @@ resource "google_project_service" "apis" {
     "cloudfunctions.googleapis.com",
     # Pub/Sub is used to handle events from Cloud Build
     "pubsub.googleapis.com",
-    # Slack webhook URL is stored in GSM for Cloud Run to use
+    # Slack webhook URL is stored in GSM for Cloud Function to use
     "secretmanager.googleapis.com"
   ])
   project = var.project_id
@@ -34,22 +40,43 @@ resource "google_service_account" "slack_notifier" {
 # Secrets
 # ------------------------------------------------------------------------------
 
-# Setup a secret to store incoming slack webhook URL in Secret Manager
+# If not provided, setup a secret to store incoming slack webhook URL in GSM
 resource "google_secret_manager_secret" "cloud_build_slack_webhook_url" {
+  count = local.secret_id_provided ? 0 : 1
+
   project   = var.project_id
-  secret_id = "cloud-build-slack-webhook-url-${var.slack_channel_name}"
+  secret_id = "cloud-build-slack-webhook-url-${local.clean_name}"
 
   replication {
     automatic = true
   }
+
+  depends_on = [
+    google_project_service.apis
+  ]
+}
+
+resource "google_secret_manager_secret_version" "cloud_build_slack_webhook_url" {
+  count = local.secret_id_provided ? 0 : 1
+
+  secret      = google_secret_manager_secret.cloud_build_slack_webhook_url.id
+  secret_data = var.slack_webhook_url
+
+  depends_on = [
+    google_project_service.apis
+  ]
 }
 
 # Give the slack notifier service account access to the secret
 resource "google_secret_manager_secret_iam_member" "slack_notifier_secret_accessor" {
   project   = var.project_id
-  secret_id = google_secret_manager_secret.cloud_build_slack_webhook_url.secret_id
+  secret_id = local.secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.slack_notifier.email}"
+
+  depends_on = [
+    google_project_service.apis
+  ]
 }
 
 
@@ -61,6 +88,10 @@ resource "google_secret_manager_secret_iam_member" "slack_notifier_secret_access
 resource "google_pubsub_topic" "cloud_builds" {
   project = var.project_id
   name    = "cloud-builds"
+
+  depends_on = [
+    google_project_service.apis
+  ]
 }
 
 
@@ -97,8 +128,8 @@ resource "google_storage_bucket_object" "cloud_build_slack_notifier_script" {
 # ------------------------------------------------------------------------------
 resource "google_cloudfunctions_function" "slack_notifier" {
   provider    = google-beta
-  name        = "slack-notifier-${regex("[0-9A-Za-z]+", google_storage_bucket_object.cloud_build_slack_notifier_script.crc32c)}" # HACK To make the function change when the script changes
-  description = "Slack Notifier"
+  name        = "cloud-build-slack-notifier-${local.clean_name}-${regex("[0-9A-Za-z]+", google_storage_bucket_object.cloud_build_slack_notifier_script.crc32c)}" # HACK To make the function change when the script changes
+  description = "Slack Notifier - ${var.name}"
   runtime     = "python39"
   project     = var.project_id
   region      = "us-central1" # Note: This can only be us-central1 according to docs
@@ -116,12 +147,13 @@ resource "google_cloudfunctions_function" "slack_notifier" {
   entry_point = "handle_cloudbuild_event"
 
   environment_variables = {
-    SECRET_ID = google_secret_manager_secret.cloud_build_slack_webhook_url.id
+    SECRET_ID = local.secret_id
   }
 
   service_account_email = google_service_account.slack_notifier.email
 
   depends_on = [
+    google_project_service.apis,
     google_storage_bucket_object.cloud_build_slack_notifier_script
   ]
 }
--- variables.tf
+++ variables.tf
@@ -3,9 +3,22 @@ variable "project_id" {
   type        = string
 }
 
-variable "slack_channel_name" {
-  description = "The Slack channel name in which to publish notifications."
+variable "name" {
+  description = "The name to use on all resources created. A good name might be the name of the Slack channel in which this notifier will publish messages."
   type        = string
+}
+
+variable "slack_webhook_url_secret_id" {
+  description = "The GSM secret for the existing Slack webhook URL. Optional - If not provided a secret will be created with the value of 'slack_webhook_url'."
+  type        = string
+  default     = ""
+}
+
+variable "slack_webhook_url" {
+  description = "The Slack webhook URL on which to publish notifications. Optional - If not provided it the existing secret_id should be passed in on 'slack_webhook_url_secret_id'."
+  type        = string
+  sensitive   = true
+  default     = ""
 }
 
 # See: https://cloud.google.com/build/docs/configuring-notifications/configure-slack#using_cel_to_filter_build_events
@@ -14,3 +27,4 @@ variable "cloud_build_event_filter" {
   type        = string
   default     = "build.substitutions[\"BRANCH_NAME\"] == \"main\""
 }
+
